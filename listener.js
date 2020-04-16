6e97b4923d32452416e7b1f8c7533da3326eb578
Ross Weijer <ross@datascopeanalytics.com>
2020-04-16 10:04:34 -0500

    Browser logs transcript of speech in console

--- listener.js
+++ listener.js
@@ -5,19 +5,33 @@ let recognition = new SpeechRecognition();
 recognition.continuous = true;
 recognition.lang = "en-US";
 recognition.interimResults = false;
+
+
 let isListening = false;
+// let resultIndex = 0
 
 recognition.onstart = function () {
-	console.log(`Am I listening?? ${isListening}`)
-	wordsHeard = ""
+  console.log(`Am I listening?? ${isListening}`)
+  wordsHeard = []
 }
+
+recognition.onresult = function (event) {
+  let latestResult = event.results[event.resultIndex]
+  if (latestResult.isFinal) {
+      wordsHeard.push(...latestResult[0].transcript.split(" "))
+      // resultIndex += 1
+      console.log(wordsHeard)
+      console.log(`Count of words: ${wordsHeard.length}`)
+    }
+}
+
 
 
 let micButton = document.getElementById("micOn")
 micButton.addEventListener("click", function () {
-	if (!isListening) {
-		console.log("Turning on Mic")
-		isListening = true
-		recognition.start()
-	}
+  if (!isListening) {
+    console.log("Turning on Mic")
+    isListening = true
+    recognition.start()
+  }
 })
